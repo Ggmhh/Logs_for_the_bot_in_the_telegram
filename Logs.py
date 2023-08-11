4b72252542472277e6801587110eb3c4d044a48a
Ggmhh <128885687+Ggmhh@users.noreply.github.com>
2023-08-11 21:59:01 +0300

    Logs.py

--- Logs.py
+++ Logs.py
@@ -86,6 +86,58 @@ def handle_message(message):
     else:
         text = "Неизвестный тип"
 
+@bot.edited_message_handler(content_types=['text', 'sticker', 'photo', 'audio', 'video', 'document', 'animation'])
+def handle_edited_message(message):
+    user_name = get_user_name(message)
+    user_id = message.from_user.id
+    date = message.edit_date
+    formatted_date = datetime.datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S')
+
+    if message.text:
+        text = message.text
+        bot.send_message(
+            CHAT_ID,
+            f"Измененное сообщение:\n"
+            f"Имя: {user_name}\n"
+            f"ID: {user_id}\n"
+            f"Сообщение: {text}\n" 
+            f"Время изменения: {formatted_date}"  
+        )
+    elif message.sticker:
+        text = "Стикер"
+        bot.send_message(
+            CHAT_ID,
+            f"Измененный стикер:\n"
+            f"Имя: {user_name}\n"
+            f"ID: {user_id}\n"
+            f"Сообщение: {text}\n" 
+            f"Время изменения: {formatted_date}"  
+        )
+        bot.send_sticker(CHAT_ID, message.sticker.file_id)
+    elif message.photo:
+        text = "Фото"
+        bot.send_message(
+            CHAT_ID,
+            f"Измененное фото:\n"
+            f"Имя: {user_name}\n"
+            f"ID: {user_id}\n"
+            f"Сообщение: {text}\n" 
+            f"Время изменения: {formatted_date}"  
+        )
+        bot.send_photo(CHAT_ID, message.photo[0].file_id)
+    elif message.animation:
+        text = "GIF"
+        bot.send_message(
+            CHAT_ID,
+            f"Измененный GIF:\n"
+            f"Имя: {user_name}\n"
+            f"ID: {user_id}\n"
+            f"Сообщение: {text}\n" 
+            f"Время изменения: {formatted_date}"  
+        )
+        bot.send_animation(CHAT_ID, message.animation.file_id)
+    # Обработка измененных сообщений для других типов
+
 def get_user_name(message):
     user = message.from_user
     if user.first_name:
